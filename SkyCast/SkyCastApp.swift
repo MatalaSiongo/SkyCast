9f751d63737439b03d2f81d0adbc437ac046cebc
MatalaSiongo <180665158+MatalaSiongo@users.noreply.github.com>
2026-08-19 17:39:04 +0200

    Initial Commit

---
+++ SkyCast/SkyCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyCastApp.swift
+//  SkyCast
+//
+//  Created by Matala on 2026-08-19.
+//
+
+import SwiftUI
+
+@main
+struct SkyCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SkyCastTests/SkyCastTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkyCastTests.swift
+//  SkyCastTests
+//
+//  Created by Matala on 2026-08-19.
+//
+
+import Testing
+@testable import SkyCast
+
+struct SkyCastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
